39a1b56a5932828e4186fb2682d03cffcc66b438
Lưu Tiến Phát <luutienphat@gmail.com>
2021-12-31 23:47:16 +0700

    Delete style.js

--- style.js
+++
@@ -1,7 +0,0 @@
-// const input = document.getElementsByClassName("input")[0];
-// const optionTable = document.getElementsByClassName("option-table")[0];
-
-// input.addEventListener("click", (e) => {
-//   document.getElementById("map").style.height = "0%";
-//   optionTable.style.height = "100vh";
-// });
